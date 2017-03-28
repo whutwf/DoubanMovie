f6693937c5e19ffd715b8104989c0f36e7e4936a
whutwf <whutwf@outlook.com>
2017-03-29 00:03:17 +0800

    清除无关注释

--- app/src/main/java/com/study/whutwf/doubanmovie/adapter/MovieTopAdapter.java
+++ app/src/main/java/com/study/whutwf/doubanmovie/adapter/MovieTopAdapter.java
@@ -2,17 +2,13 @@ package com.study.whutwf.doubanmovie.adapter;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.support.v7.widget.AppCompatRatingBar;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.study.whutwf.doubanmovie.R;
@@ -31,11 +27,6 @@ public class MovieTopAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     private List<MovieItem> mMovieItemList;
     private ImageDownloader<MovieTopItemViewHolder> mMovieTopItemViewHolderImageDownloader;
-
-//    public MovieTopAdapter(List<MovieItem> itemList, ImageDownloader<MovieTopItemViewHolder> imageDownloader) {
-//        mMovieItemList = itemList;
-//        mMovieTopItemViewHolderImageDownloader = imageDownloader;
-//    }
 
     public MovieTopAdapter(List<MovieItem> movieItemList, ImageDownloader<MovieTopItemViewHolder> imageDownloader) {
         mMovieItemList = movieItemList;
@@ -137,69 +128,4 @@ public class MovieTopAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         context.startActivity(Intent.createChooser(shareIntent, context.getString(R.string.movie_share_to)));
     }
 
-    private static class MovieTopItemViewHolder extends RecyclerView.ViewHolder
-            implements View.OnClickListener {
-
-        private TextView mMovieNameTextView;
-        private TextView mMovieYearTextView;
-        private TextView mMovieOriginalTitleTextView;
-        private TextView mMovieScoreTextView;
-        private ImageView mMovieCoverImageView;
-        private AppCompatRatingBar mAppCompatRatingBar;
-        private ImageView mOverflowMenuImageView;
-
-        private MovieTopItemViewHolder.ClickResponseListener mClickResponseListener;
-
-        public MovieTopItemViewHolder(View itemView, MovieTopItemViewHolder.ClickResponseListener clickResponseListener) {
-            super(itemView);
-
-            mClickResponseListener = clickResponseListener;
-
-            mMovieNameTextView = (TextView) itemView.findViewById(R.id.top_movie_item_name);
-            mMovieYearTextView = (TextView) itemView.findViewById(R.id.top_movie_item_date);
-            mMovieCoverImageView = (ImageView) itemView.findViewById(R.id.top_movie_item_cover);
-            mAppCompatRatingBar = (AppCompatRatingBar) itemView.findViewById(R.id.rating_bar_hots);
-            mMovieOriginalTitleTextView = (TextView) itemView.findViewById(R.id.top_movie_item_original_name);
-            mMovieScoreTextView = (TextView) itemView.findViewById(R.id.top_movie_item_score);
-            mOverflowMenuImageView = (ImageView) itemView.findViewById(R.id.top_card_share_overflow);
-
-            mOverflowMenuImageView.setOnClickListener(this);
-            itemView.setOnClickListener(this);
-
-        }
-
-        public static MovieTopItemViewHolder newInstance(View parent, MovieTopItemViewHolder.ClickResponseListener clickResponseListener) {
-            return new MovieTopItemViewHolder(parent, clickResponseListener);
-        }
-
-        public void setTopMovieItem(MovieItem movieItem) {
-            mMovieNameTextView.setText(movieItem.getTitle());
-            mMovieOriginalTitleTextView.setText(movieItem.getOriginalTitle());
-            mMovieYearTextView.setText(movieItem.getYear());
-            mMovieScoreTextView.setText(movieItem.getRating());
-            mAppCompatRatingBar.setRating(Float.parseFloat(movieItem.getRating()) / 2);
-
-        }
-
-        public void setTopMovieCover(Drawable drawable) {
-            mMovieCoverImageView.setImageDrawable(drawable);
-        }
-
-        @Override
-        public void onClick(View view) {
-            if (view == mOverflowMenuImageView) {
-                mClickResponseListener.onOverflowClick(view, getAdapterPosition());
-            } else {
-                mClickResponseListener.onWholeClick(getAdapterPosition());
-            }
-        }
-
-        /**
-         * 注意接口的使用，可以针对不同的场景不同实现
-         */
-        public interface ClickResponseListener {
-            void onWholeClick(int position);
-            void onOverflowClick(View v ,int position);
-        }
-    }
 }
--- app/src/main/java/com/study/whutwf/doubanmovie/adapter/MovieTopItemViewHolder.java
+++ app/src/main/java/com/study/whutwf/doubanmovie/adapter/MovieTopItemViewHolder.java
@@ -1,6 +1,7 @@
 package com.study.whutwf.doubanmovie.adapter;
 
 import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatRatingBar;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
@@ -13,30 +14,67 @@ import com.study.whutwf.doubanmovie.bean.MovieItem;
  * Created by whutwf on 17-3-13.
  */
 
-public class MovieTopItemViewHolder extends RecyclerView.ViewHolder {
+public class MovieTopItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
     private TextView mMovieNameTextView;
     private TextView mMovieYearTextView;
+    private TextView mMovieOriginalTitleTextView;
+    private TextView mMovieScoreTextView;
     private ImageView mMovieCoverImageView;
+    private AppCompatRatingBar mAppCompatRatingBar;
+    private ImageView mOverflowMenuImageView;
 
-    public MovieTopItemViewHolder(View itemView) {
+    private MovieTopItemViewHolder.ClickResponseListener mClickResponseListener;
+
+    public MovieTopItemViewHolder(View itemView, MovieTopItemViewHolder.ClickResponseListener clickResponseListener) {
         super(itemView);
+
+        mClickResponseListener = clickResponseListener;
+
         mMovieNameTextView = (TextView) itemView.findViewById(R.id.top_movie_item_name);
         mMovieYearTextView = (TextView) itemView.findViewById(R.id.top_movie_item_date);
         mMovieCoverImageView = (ImageView) itemView.findViewById(R.id.top_movie_item_cover);
+        mAppCompatRatingBar = (AppCompatRatingBar) itemView.findViewById(R.id.rating_bar_hots);
+        mMovieOriginalTitleTextView = (TextView) itemView.findViewById(R.id.top_movie_item_original_name);
+        mMovieScoreTextView = (TextView) itemView.findViewById(R.id.top_movie_item_score);
+        mOverflowMenuImageView = (ImageView) itemView.findViewById(R.id.top_card_share_overflow);
+
+        mOverflowMenuImageView.setOnClickListener(this);
+        itemView.setOnClickListener(this);
+
     }
 
-    public static MovieTopItemViewHolder newInstance(View parent) {
-        return new MovieTopItemViewHolder(parent);
+    public static MovieTopItemViewHolder newInstance(View parent, MovieTopItemViewHolder.ClickResponseListener clickResponseListener) {
+        return new MovieTopItemViewHolder(parent, clickResponseListener);
     }
 
     public void setTopMovieItem(MovieItem movieItem) {
         mMovieNameTextView.setText(movieItem.getTitle());
+        mMovieOriginalTitleTextView.setText(movieItem.getOriginalTitle());
         mMovieYearTextView.setText(movieItem.getYear());
+        mMovieScoreTextView.setText(movieItem.getRating());
+        mAppCompatRatingBar.setRating(Float.parseFloat(movieItem.getRating()) / 2);
+
     }
 
     public void setTopMovieCover(Drawable drawable) {
         mMovieCoverImageView.setImageDrawable(drawable);
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view == mOverflowMenuImageView) {
+            mClickResponseListener.onOverflowClick(view, getAdapterPosition());
+        } else {
+            mClickResponseListener.onWholeClick(getAdapterPosition());
+        }
+    }
+
+    /**
+     * 注意接口的使用，可以针对不同的场景不同实现
+     */
+    public interface ClickResponseListener {
+        void onWholeClick(int position);
+        void onOverflowClick(View v ,int position);
+    }
 }
