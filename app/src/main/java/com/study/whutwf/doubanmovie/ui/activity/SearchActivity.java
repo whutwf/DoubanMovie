83c3c3fbc0afa857aa23316cedfc4bcea6a8cf9f
whutwf <whutwf@outlook.com>
2017-03-29 17:19:03 +0800

    初始化查询框架代码

--- app/src/main/java/com/study/whutwf/doubanmovie/ui/activity/SearchActivity.java
+++ app/src/main/java/com/study/whutwf/doubanmovie/ui/activity/SearchActivity.java
@@ -1,8 +1,7 @@
 package com.study.whutwf.doubanmovie.ui.activity;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.support.v7.widget.SearchView;
-import android.widget.RelativeLayout;
 
 import com.study.whutwf.doubanmovie.R;
 import com.study.whutwf.doubanmovie.ui.fragment.MovieSearchFragment;
@@ -11,31 +10,27 @@ import com.study.whutwf.doubanmovie.ui.fragment.MovieSearchFragment;
  * Created by whutwf on 17-3-28.
  */
 
-public class SearchActivity extends SingleFragmentActivity {
+public class SearchActivity extends BaseActivity {
 
     private SearchView mSearchView;
+    private MovieSearchFragment mSearchNewsFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mSearchNewsFragment = MovieSearchFragment.newInstance();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_frame, mSearchNewsFragment)
+                .commit();
+    }
 
     @Override
     protected void initView() {
         super.initView();
         //这里有问题
         setTitle("");
-        mSearchView = new SearchView(this);
-        mSearchView.setQueryHint(getResources().getString(R.string.search_hint));
-        //默认展开
-        mSearchView.setIconifiedByDefault(false);
-//        mSearchView.
-        RelativeLayout relativeLayout = new RelativeLayout(this);
-        relativeLayout.addView(mSearchView);
-
-        mToolbar.addView(relativeLayout);
-        setSupportActionBar(mToolbar);
         //这句话的作用,目前没有起作用
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    protected Fragment createFragment() {
-        return MovieSearchFragment.newInstance();
     }
 }
--- app/src/main/java/com/study/whutwf/doubanmovie/ui/fragment/MovieSearchFragment.java
+++ app/src/main/java/com/study/whutwf/doubanmovie/ui/fragment/MovieSearchFragment.java
@@ -1,5 +1,13 @@
 package com.study.whutwf.doubanmovie.ui.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.study.whutwf.doubanmovie.R;
 import com.study.whutwf.doubanmovie.support.Constants;
 
 /**
@@ -13,7 +21,34 @@ public class MovieSearchFragment extends MovieBaseFragment {
         paramsHashMap.put("url", Constants.Urls.DOUBAN_MOVIE_TOP250);
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     public static MovieSearchFragment newInstance() {
         return new MovieSearchFragment();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.search_movie, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.menu_movie_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
     }
 }
